5675ab8d40809b68d8579599017825d02990531b
cshcshan <csh771355@gmail.com>
2015-11-09 01:02:34 +0800

    1. 使用GameObject做為Leap Motion的鼠標。 2. 替他加入Collider(開啟isTrigger，並設定z軸size在與GameObject堆疊時，足以觸發Trigger)和Rigidbody。 3. 在mouse觸發Trigger時，透過delegate和event回傳到main script處理。

---
+++ Assets/Scripts/LeapMotionTest.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapMotionTest : MonoBehaviour {
+	public delegate void TriggerDelegate(Collider otherObject);
+
+	public GameObject mouse;
+	public GameObject leftMouse;
+	public GameObject rightMouse;
+	
+	Controller leapController;
+
+	void Awake() {
+		Mouse m = mouse.GetComponent<Mouse> ();
+		if (m == null) {
+			m = mouse.AddComponent<Mouse>();
+			m.TriggerEvent += TriggerObject;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (leapController == null) {
+			Debug.Log("START");
+
+			leapController = new Controller();
+			StartCoroutine(DetectHand());
+		} else {
+			Debug.Log("無法偵測到Leap Motion裝置");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void TriggerObject(Collider other) {
+		Debug.Log ("TriggerObject: " + other.name);
+	}
+
+	IEnumerator DetectHand() {
+		// THUMB、INDEX、MIDDLE、RING、PINKY
+		// Leap Motion Interaction Box: {width: 235mm, height: 235mm(start: 82.5mm); depth: 147mm}
+		// Unity Position Range: {x: -8 ~ 8, y: -5 ~ 5}
+
+		int LEAP_WIDTH = 235;
+		int LEAP_HEIGHT = 235;
+		int LEAP_DEPTH = 147;
+
+		int CAMERA_WIDTH = 16;
+		int CAMERA_HEIGHT = 10;
+
+		float MAXX = (float)(CAMERA_WIDTH - leftMouse.transform.localScale.x) * 0.5f;
+		float MAXY = (float)(CAMERA_HEIGHT - leftMouse.transform.localScale.y) * 0.5f;
+
+		Frame frame;
+		Vector3 fingerPosition;
+
+		while (true) {
+			if (leapController != null) {
+				frame = leapController.Frame ();
+				foreach (Hand hand in frame.Hands) {
+					foreach (Finger finger in hand.Fingers) {
+						switch (finger.Type) {
+							case Finger.FingerType.TYPE_INDEX:
+							fingerPosition = new Vector3((float)(finger.TipPosition.x / (LEAP_WIDTH * 0.5f)) * CAMERA_WIDTH, 
+							                             (float)((finger.TipPosition.y - 82.5f) / LEAP_HEIGHT) * CAMERA_HEIGHT - CAMERA_HEIGHT * 0.5f, 
+							                             (float)(finger.TipPosition.z / (LEAP_DEPTH * 0.5f)) * 10.0f);
+							if (fingerPosition.x > MAXX) fingerPosition.x = MAXX;
+							if (fingerPosition.x < -MAXX) fingerPosition.x = -MAXX;
+							if (fingerPosition.y > MAXY) fingerPosition.y = MAXY;
+							if (fingerPosition.y < -MAXY) fingerPosition.y = -MAXY;
+							fingerPosition.z = 0;
+							if (hand.IsLeft) {
+								leftMouse.transform.position = fingerPosition;
+							} else if (hand.IsRight) {
+								//rightMouse.transform.localPosition = fingerPosition;
+							}
+							mouse.transform.position = fingerPosition;
+							break;
+						default:
+							break;
+						}
+					}
+				}
+			}	
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+	public delegate void TriggerDelegate(Collider otherObject);
+	public event TriggerDelegate TriggerEvent;
+
+	// Use this for initialization
+	void Start () {
+		BoxCollider collider = gameObject.GetComponent<BoxCollider> ();
+		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody> ();
+		Vector3 colliderSize;
+
+		if (collider == null) {
+			collider = gameObject.AddComponent<BoxCollider>();
+		}
+		if (rigidbody == null) {
+			rigidbody = gameObject.AddComponent<Rigidbody>();
+		}
+		colliderSize = collider.size;
+		colliderSize.z = 10.0f;
+		collider.size = colliderSize;
+		collider.isTrigger = true;
+		rigidbody.useGravity = false;
+		rigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) { 
+		TriggerEvent(other);
+	}
+}
